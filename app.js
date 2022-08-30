68c8c7ba4d3e587b208633bcb1159b7333925d53
duraznoj <sashka.warner@gmail.com>
2022-08-30 00:24:29 -0700

    Start adding operation evaluation and button functionality

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+//////// GLOBAL VARIABLES ////////
+let currentNum = '';
+let nextNum = '';
+let currentOp = '';
+let waitForOperator = true;
+let firstStatementEvaluated = false;
+//let digitBuffer = [];
+
+const display = document.querySelector(".display");
+
+//const digitButtons = document.querySelectorAll(".digit > button");
+const buttons = document.querySelectorAll("button");
+
+
+//////// FUNCTIONS ////////
+
 const add = function(a, b) {
   return a + b;
 };
@@ -61,15 +77,108 @@ const operate = (num1, num2, operator) => {
     case '-':
       result = subtract(num1, num2);
       break;
-    case '*':
+    case 'X':
       result = multiply([num1, num2]);
       break;
-    case '/':
+    case '÷':
       result = divide([num1, num2]);
       break;
   }
+  
+  display.textContent = result;
   return result;
 }
 
 //console.log(operate(3,9,'/'));
 //console.log(divide([3,9,9,2]));
+
+
+
+//////// MAIN ////////
+
+//want to get input of numbers only to update display
+//want to concatenate numbers together in string 
+//then want to perform operation on the numbers we have stored when the execute button is called.....
+//could have array of objects...
+/*[
+  {
+    num1: 2,
+    num2: 4,
+    op: '+'
+  },
+]*/
+
+
+function processInput() {
+
+  const input = this.textContent;
+
+  const inputType = this.parentElement.className;
+  //console.log(inputType);
+
+  //digitInput = Number(this.textContent).toFixed(2); //store input data
+  if(inputType === "digit" && !firstStatementEvaluated) {
+
+    if(waitForOperator) {
+      console.log("currentNum update")
+      currentNum += input; //add numbers to string
+      display.textContent = currentNum; //update display
+
+    } else {
+      console.log("nextNum update")
+      nextNum += input; //add numbers to a string
+      display.textContent = nextNum; //update display
+
+    }
+
+    //display.textContent = input; //update display
+
+  } else if(inputType === "operator"){
+
+    switch(input) {
+      case "=":
+        console.log("equals operator")
+        console.log(Number(currentNum), Number(nextNum), currentOp);
+        currentNum = String(operate(Number(currentNum), Number(nextNum), currentOp)); //set output to currentNum
+        nextNum = ''; //reset nextNum
+        console.log(Number(currentNum), Number(nextNum), currentOp);
+        waitForOperator = true;
+        firstStatementEvaluated = true;
+        break;
+
+      case "C":
+        if(!firstStatementEvaluated) {
+          if(display.textContent.length === 1) {
+            display.textContent = '0';
+            waitForOperator ? currentNum = '': nextNum = '';
+          } else {
+            display.textContent = display.textContent.slice(0, -1) //remove from display
+            currentNum = currentNum.slice(0, -1); //remove from currentNum;
+          }
+
+        }
+        break;
+
+      case "AC":
+        display.textContent = '0';
+        currentNum = '';
+        nextNum = '';
+        currentOp = '';
+        firstStatementEvaluated = false;
+        break;
+
+      default:
+        console.log("other operator");
+        currentOp = input;
+        waitForOperator = false;
+        firstStatementEvaluated = false;
+
+    } 
+    
+  }
+  
+}
+
+buttons.forEach((elem) => {
+  elem.addEventListener('click', processInput);
+});
